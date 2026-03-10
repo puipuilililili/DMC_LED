1d73cba660d0435c09cd06edc07f26c95bcf4163
puip <9000rubbish@gmail.com>
2026-03-11 00:34:46 +0900

    fix:更新忘れによるconflictの修正

---
+++ state.py
@@ -0,0 +1,21 @@
+import asyncio
+from led_controller import LEDController
+from osc_server import OSCServer
+from bpm_client import BPMClient
+
+class AppState:
+    def __init__(self):
+        self.MasterBpm = 140
+        self.last_led_state = {
+            "BPM": 140,
+            "multiplier": 1,
+            "white_multiplier": 1,
+            "white": 0,
+            "brightness": 100
+        }
+        self.queues = set()
+        self.led = LEDController()
+        self.oscserver = OSCServer(ip = "127.0.0.1", port=7000)
+        self.BPMCient =BPMClient(url = "http://127.0.0.1:17081/params.json", polling_interval=0.1)
+
+state = AppState()
